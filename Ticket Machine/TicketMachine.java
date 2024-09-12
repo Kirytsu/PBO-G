404dd241c3688911c4c3f33aa56e3a2c005ba7c2
Nathanael Valen Susilo <natan.valen78@gmail.com>
2024-09-12 18:59:49 +0700

    implement new functions
    add the instance variables quantity and price total, also add new function to withdraw all balance left

--- Ticket Machine/TicketMachine.java
+++ Ticket Machine/TicketMachine.java
@@ -3,13 +3,15 @@
  * Write a description of class TicketMachine here.
  *
  * @author Nathanael Valen Susilo
- * @version 0.2
+ * @version 0.5
  */
 public class TicketMachine
 {
     // instance variables - replace the example below with your own
     private int price;
     private int balance;
+    private int quantity;
+    private int priceTotal;
     private int total;
     
     /**
@@ -21,6 +23,8 @@ public class TicketMachine
         if (ticketCost > 0){
             price = ticketCost;
             balance = 0;
+            quantity = 1;
+            priceTotal = price; // default quantity is 1
             total = 0;
         }
         else {
@@ -42,6 +46,10 @@ public class TicketMachine
     public int getBalance(){
         return balance;
     }
+
+    public int getRevenue(){
+        return total;
+    }
     
     public void insertMoney(int amount){
         if (amount > 0){
@@ -51,22 +59,34 @@ public class TicketMachine
             System.out.println("Amount cannot be negative : " + amount);
         }
     }
+
+    public void chooseQuantity(int qty){
+        quantity = qty;
+        priceTotal = price * qty;
+    }
     
+    public void withdrawBalance(){
+        System.out.println("Balance withdrawed : " + balance);
+        balance -= balance;
+    }
+
     public void printTicket(){
-        if (balance >= price) {
+        if (balance >= priceTotal) {
             System.out.println("######################");
             System.out.println("# The BlueJ Line");
             System.out.println("# Ticket");
             System.out.println("# " + price + " cents.");
+            System.out.println("# Valid for : " + (priceTotal/price)+ " usage");
             System.out.println("######################");
             System.out.println();
             
-            total = total + price;
-            balance = balance - price;
+            total = total + priceTotal;
+            balance = balance - priceTotal;
+            priceTotal = price;
         }
         else {
             System.out.println("You must insert at least : " + 
-                            (price - balance) + " more cents.");
+                            (priceTotal - balance) + " more cents.");
         }
     }
 }
